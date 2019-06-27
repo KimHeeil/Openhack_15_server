33d7f1d56d48c71ee128304f49497978b2e93cce
choi soo lyn <lynever@naver.com>
2019-06-27 14:40:37 +0900

    db 연결

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,28 @@
+var knex = require('../lib/knex');
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  knex.raw('select * from humaninfo;')
+  .then(function (result) {
+    console.log(result);
+    res.json(result[0]);
+  })
+  .catch(function (err) {
+    res.json(err);
+  })
 });
 
+router.get('/:id', function(req, res, next) {
+  var id = req.params.id;
+  knex.raw('select * from humaninfo where seq = ?;', [id])
+  .then(function (result) {
+    console.log(result);
+    res.json(result[0][0]);
+  })
+  .catch(function (err) {
+    res.json(err);
+  })
+});
 module.exports = router;
